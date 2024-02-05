3b94547d453c4bb1dbc854b8dddc26087b82dc53
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-02-05 10:12:29 +0100

    Add Vuex + store + related functions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 
 import router from './router'
+import store from './store/index.js'
 import App from './App.vue'
 
 const app = createApp(App)
 
 // ╾──────────────────────────────╼ ROUTER ╾────────────────────────────╼
 app.use(router)
+
+// ╾──────────────────────────────╼ STORE ╾──────────────────────────────╼
+app.use(store)
 
 // ╾────────────────────────────────╼ APP ╾────────────────────────────────╼
 app.mount('#app')
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,17 +3,31 @@
   <section>
     <div class="controls">
       <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
+      <router-link to="/register">
+        Register as Coach
+      </router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }} {{ coach.lastName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <!-- ______________________________________________________________________ -->
 <script>
-export default {}
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/allCoaches']
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches']
+    }
+  }
+}
 </script>
 
 <!-- ______________________________________________________________________ -->
