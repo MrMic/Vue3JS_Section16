eb6ab6edd589eab04284a44d02fc758ee9177daa
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-02-07 10:45:41 +0100

    Add: BaseCard + BaseButton + BaseBadge Components

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store/index.js'
 import App from './App.vue'
 
+import BaseCard from './components/ui/BaseCard.vue'
+import BaseButton from './components/ui/BaseButton.vue'
+import BaseBadge from './components/ui/BaseBadge.vue'
+
 const app = createApp(App)
 
 // ╾──────────────────────────────╼ ROUTER ╾────────────────────────────╼
@@ -11,6 +15,11 @@ app.use(router)
 
 // ╾──────────────────────────────╼ STORE ╾──────────────────────────────╼
 app.use(store)
+
+// ╾────────────────────────╼ GLOBAL COMPONENT ╾─────────────────────╼
+app.component('BaseCard', BaseCard)
+app.component('BaseButton', BaseButton)
+app.component('BaseBadge', BaseBadge)
 
 // ╾────────────────────────────────╼ APP ╾────────────────────────────────╼
 app.mount('#app')
