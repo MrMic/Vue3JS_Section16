f90f69e4e7f31a4f01d53f673f8d3c9e67f28430
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-02-13 09:54:58 +0100

    Created the Coach Form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,19 @@
-<template>REGISTER COACH</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a Coach now!</h2>
+      <coach-form />
+    </base-card>
+  </section>
+</template>
+
+<!-- ______________________________________________________________________ -->
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  name: 'CoachRegistration',
+  components: {
+    CoachForm
+  }
+};
+</script>
