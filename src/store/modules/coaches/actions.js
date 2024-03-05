dd5ea4bc76f4b29c4d0e2e367ce78e209783ac07
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-03-05 10:45:07 +0100

    (feat) Attaching the Token to outgoing Requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,8 +10,10 @@ export default {
       areas: data.areas
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://vue-http-demo-56d45-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://vue-http-demo-56d45-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=${token}`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -36,8 +36,9 @@ export default {
   // ______________________________________________________________________
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://vue-http-demo-56d45-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+      `https://vue-http-demo-56d45-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json?auth=${token}`
     );
     const responseData = await response.json();
 
